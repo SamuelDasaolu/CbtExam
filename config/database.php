fd4527a4f90c60cf7bc0b41796d4836ab1bf2b3e
SamuelDasaolu <59895038+SamuelDasaolu@users.noreply.github.com>
2022-03-24 19:37:56 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,23 +1,25 @@
 <?php
 
-// return [
-//     "DBHOST" => "us-cdbr-east-02.cleardb.com",
-
-//     "DBPASS" => "449c4d61",
-
-//     "DBUSER" => "b1a3255f56d4a4",
-
-//     "DBNAME" => "heroku_33ac80e09e697b9",
-// ];
-
 return [
-    "DBHOST" => "ec2-54-157-234-29.compute-1.amazonaws.com",
+    "DBHOST" => "http://db4free.net/",
 
-    "DBPASS" => "3193407753c309de6d805b56bf1b1b9cc0dfda70772e6c7f87eb86daa35e32c6",
+    "DBPASS" => "password",
 
-    "DBUSER" => "wdsbntrykrxisx",
+    "DBUSER" => "b1a3255f56d4a4",
 
-    "DBNAME" => "d4rg9rmpa4q9or",
+    "DBNAME" => "sammy_db",
     
-    "DBPORT" => "5432",
+    "DBPORT" => "3306",
 ];
+
+// return [
+//     "DBHOST" => "ec2-54-157-234-29.compute-1.amazonaws.com",
+
+//     "DBPASS" => "3193407753c309de6d805b56bf1b1b9cc0dfda70772e6c7f87eb86daa35e32c6",
+
+//     "DBUSER" => "wdsbntrykrxisx",
+
+//     "DBNAME" => "d4rg9rmpa4q9or",
+    
+//     "DBPORT" => "5432",
+// ];

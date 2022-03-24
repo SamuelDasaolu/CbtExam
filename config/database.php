90698e10121af4965820fbcaec9241ab78b0df22
SamuelDasaolu <59895038+SamuelDasaolu@users.noreply.github.com>
2022-03-24 17:44:49 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,11 +1,21 @@
 <?php
 
+// return [
+//     "DBHOST" => "us-cdbr-east-02.cleardb.com",
+
+//     "DBPASS" => "449c4d61",
+
+//     "DBUSER" => "b1a3255f56d4a4",
+
+//     "DBNAME" => "heroku_33ac80e09e697b9",
+// ];
+
 return [
-    "DBHOST" => "us-cdbr-east-02.cleardb.com",
+    "DBHOST" => "ec2-54-157-234-29.compute-1.amazonaws.com",
 
-    "DBPASS" => "449c4d61",
+    "DBPASS" => "3193407753c309de6d805b56bf1b1b9cc0dfda70772e6c7f87eb86daa35e32c6",
 
-    "DBUSER" => "b1a3255f56d4a4",
+    "DBUSER" => "wdsbntrykrxisx",
 
-    "DBNAME" => "heroku_33ac80e09e697b9",
+    "DBNAME" => "d4rg9rmpa4q9or",
 ];
